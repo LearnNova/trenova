0a5b0316e53a07e702c98b842aee7dabd5687cb4
victor297 <davidvictor297@gmail.com>
2024-03-29 16:00:52 +0100

    sidenav

--- src/components/sidebar/components/Links.jsx
+++ src/components/sidebar/components/Links.jsx
@@ -24,40 +24,77 @@ export function SidebarLinks(props) {
         // route.layout === "/rtl"
       ) {
         return (
-          <Link
-            onClick={onClose}
-            key={index}
-            to={route.layout + "/" + route.path}
-          >
-            <div className="relative mb-3 flex hover:cursor-pointer">
-              <li
-                className="my-[3px] flex cursor-pointer items-center px-8"
-                key={index}
-              >
-                <span
-                  className={`${
-                    activeRoute(route.path) === true
-                      ? "font-bold text-gold dark:text-white"
-                      : "font-medium text-gray-600"
-                  }`}
+          <>
+            <Link
+              onClick={onClose}
+              key={index}
+              to={route.layout + "/" + route.path}
+              className="sm:hidden"
+            >
+              <div className="relative mb-3 flex hover:cursor-pointer">
+                <li
+                  className="my-[3px] flex cursor-pointer items-center px-8"
+                  key={index}
                 >
-                  {route.icon ? route.icon : <DashIcon />}{" "}
-                </span>
-                <p
-                  className={`leading-1 ml-4 flex ${
-                    activeRoute(route.path) === true
-                      ? "font-bold text-navy-700 dark:text-white"
-                      : "font-medium text-gray-600"
-                  }`}
+                  <span
+                    className={`${
+                      activeRoute(route.path) === true
+                        ? "font-bold text-gold dark:text-white"
+                        : "font-medium text-gray-600"
+                    }`}
+                  >
+                    {route.icon ? route.icon : <DashIcon />}{" "}
+                  </span>
+                  <p
+                    className={`leading-1 ml-4 flex ${
+                      activeRoute(route.path) === true
+                        ? "font-bold text-navy-700 dark:text-white"
+                        : "font-medium text-gray-600"
+                    }`}
+                  >
+                    {route.name}
+                  </p>
+                </li>
+                {activeRoute(route.path) ? (
+                  <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-gold dark:bg-brand-400" />
+                ) : null}
+              </div>
+            </Link>
+            <Link
+              key={index}
+              to={route.layout + "/" + route.path}
+              className="hidden sm:block"
+            >
+              <div className="relative mb-3 flex hover:cursor-pointer">
+                <li
+                  className="my-[3px] flex cursor-pointer items-center px-8"
+                  key={index}
                 >
-                  {route.name}
-                </p>
-              </li>
-              {activeRoute(route.path) ? (
-                <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-gold dark:bg-brand-400" />
-              ) : null}
-            </div>
-          </Link>
+                  <span
+                    className={`${
+                      activeRoute(route.path) === true
+                        ? "font-bold text-gold dark:text-white"
+                        : "font-medium text-gray-600"
+                    }`}
+                  >
+                    {route.icon ? route.icon : <DashIcon />}{" "}
+                  </span>
+                  <p
+                    className={`leading-1 ml-4 flex ${
+                      activeRoute(route.path) === true
+                        ? "font-bold text-navy-700 dark:text-white"
+                        : "font-medium text-gray-600"
+                    }`}
+                  >
+                    {route.name}
+                  </p>
+                </li>
+                {activeRoute(route.path) ? (
+                  <div className="absolute right-0 top-px h-9 w-1 rounded-lg bg-gold dark:bg-brand-400" />
+                ) : null}
+              </div>
+            </Link>
+          </>
         );
       }
     });
