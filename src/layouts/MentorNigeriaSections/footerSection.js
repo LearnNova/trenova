2ce63bb212338dbd2b58b28b89bb669694825508
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2024-06-20 12:32:42 +0100

    edited footer.js

--- src/layouts/MentorNigeriaSections/footerSection.js
+++ src/layouts/MentorNigeriaSections/footerSection.js
@@ -11,13 +11,13 @@ const FooterSection = () => {
                   <div className='location'>MentorNigeria Academy and LearnNova Learning Support Centre are members of TreNova Group.</div>
                   <div className='location'>7B, Ogunsiji Close, Off Allen Avenue, Ikeja, Lagos.</div>
                   <a href="tel:08179361381"><b>0817 936 1381 (Whatsapp-Only)</b></a>
-                  <div style={{color: '#4151E5'}}><a href='https://www.learnnova.ng' target='blank'><b>www.learnnova.ng</b></a></div>
+                  <div style={{color: '#4151E5', marginBottom: '10px'}}><a href='https://www.learnnova.ng' target='blank'><b>www.learnnova.ng</b></a></div>
               </div>
               <div className='f-section-col phone'>
                   <a href="tel:08179361381">Call Now</a>
                   <a href="tel:09137819540"><b>0913 781 9540</b></a>
                   <a href="tel:07044086794"><b>0704 408 6794</b></a>
-                  <a href="tel:09093930074"><b>0909 3930074</b></a>
+                  <a href="tel:09093930074"><b>0909 393 0074</b></a>
   
               </div>
             
