9491a3a83b947646bcfd1bb7ef232ddb2973c19f
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-01-02 20:50:27 +0100

    Changed some things

--- src/layouts/MentorNigeriaSections/SchoolGrowth.js
+++ src/layouts/MentorNigeriaSections/SchoolGrowth.js
@@ -12,26 +12,26 @@ const SchoolGrowth = () => {
         <div className='school-growth-section'>
             <div className='main-section'>
                 <div className='title-container'>
-                    <div><h1 className='bold'>Join LearnNova's Learning Support <br className='break-line'/>& School Growth System to Access These Exclusive <span className='bold yellow-text'>Benefits!</span></h1></div>
+                    <div><h1 className='bold'>Subscribe to <span className='second-text'>LearnNova Learning Support <br className='break-line'/>System & Enjoy the Following <span className='bold yellow-text'>Benefits!</span></span></h1></div>
                 </div>
                 <div className='text-body-container'>
                     <div className='text-body'>
                         <ul>
-                            <li>Enrol 45 pupils/students on the Home Learning App at no extra cost - <span className='red-text bold small-text'>(Valued at ₦225,000 per term)</span></li>
-                            <li>Installation of Diction and Phonetics Studio for Elementary School <span className='red-text bold'>or</span> Mathematics Studio for Senior Secondary School in your School - <span className='red-text bold small-text'>(Valued at ₦250,000 per term)</span></li>
-                            <li>Installation of LearnNova Creative Life Studio Desktop App - <span className='red-text bold small-text'>(Valued at ₦249,000 per term)</span></li>
-                            <li>Free Access to TreNova Virtual Consulting, Mentorship & Coaching Sessions - <span className='red-text bold small-text'>(Valued at ₦150,000 per term)</span></li>
-                            <li>Complimentary Access to TreNova Academy Online Training Platform - <span className='red-text bold small-text'>(Valued at ₦75,000 per term)</span></li>
-                            <li>Enjoy a special Rate of <span className='red-text bold'>₦1,650</span> per user per term for additional Home Learning App - <span className='red-text bold'>save 67%</span> from the standard fee of <span className='red-text bold'>₦5,000</span> per term! - <span className='red-text bold small-text'>(Valued at ₦3,350 Saving per user per term)</span></li> 
-                            <li>Opportunity to suggest new creative life skill for LearnNova to develop.</li>
+                            <li>Installation of Diction and Phonetics Studio for Elementary School <span className='red-text bold'>or</span> Mathematics Studio for Senior Secondary School on your school computer desktop - <span className='red-text bold small-text'>(Valued at ₦250,000 per term)</span></li>
+                            <li>Installation of Creative & Vocational Skills Development Studio on your school computer desktop - <span className='red-text bold small-text'>(Valued at ₦249,000 per term)</span></li>
+                            <li>Enrollment for <span className='bold'>45</span> pupils/students on our Home Learning App - <span className='red-text bold small-text'>(Valued at ₦225,000 per term)</span></li>
+                            <li>Refer <span className='bold'>1 school</span> to LearnNova and <span className='bold'>Enjoy Free</span> enrollment for <span className='bold'>25</span> additional pupils/students on the Home Learning App or purchase extra unit at a discounted rate - <span className='red-text bold small-text'>(Valued at ₦125,000)</span></li> 
+                            <li>Free Access to TreNova virtual consulting & coaching sessions - <span className='red-text bold small-text'>(Valued at ₦150,000 per term)</span></li>
+                            <li>Complimentary access to TreNova Academy online training platform - <span className='red-text bold small-text'>(Valued at ₦100,000 per term)</span></li>
+                            <li>Opportunity to suggest new creative & vocational skill for LearnNova to develop.</li>
                         </ul>
                     </div>
                     <div className='price-body'>
                         <span className='title'>Subscription Fee:</span>
-                        <span className='amount bold deep-purple'>₦129,999 per term</span>
+                        <span className='amount bold deep-purple'>₦149,999 per term</span>
                     </div>
                     <div className='note-body red-text bold'>
-                        Schools that subscribe & pay before the end of Jan 2025 will get it for <span className='deep-purple'>₦99,999 per term</span> - save 23.08% !
+                        Schools that subscribe & pay before the end of <span className='deep-purple'>PROMO</span> will get it for <span className='deep-purple'>₦99,999 per term</span> - save 33.33% !
                     </div>
                     <div className='interest-body'>
                         <a className='bold' href='#application-form-section'>Are you interested? Fill the form below</a>
@@ -69,7 +69,7 @@ const SchoolGrowth = () => {
 
         <div className='contact-body small-screen-contact' style={{paddingLeft: '.8em'}}>
             <h3 className='deep-purple bold'>For more inquiries, call:</h3>
-            <p className='bold'><a href='tel:07030148694'>07030148694;</a><a href='tel:07044086794'>07044086794;</a><a href='tel:09137819540'>09137819540</a></p>
+            <p className='bold'><a href='tel:07030148694'>07030148694;</a><a href='tel:09137819540'>09137819540</a></p>
         </div>
     </section>
     
