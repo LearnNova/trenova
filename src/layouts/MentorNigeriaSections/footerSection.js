b9b08e14e270dfa37e4ed5331d9f9af993c08ac8
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-01-29 11:34:01 +0100

    A

--- src/layouts/MentorNigeriaSections/footerSection.js
+++ src/layouts/MentorNigeriaSections/footerSection.js
@@ -11,15 +11,14 @@ const FooterSection = () => {
               <div className='location'>7B, Ogunsiji Close, Off Allen Avenue, Ikeja, Lagos.</div>
           </div>
           <div className='f-section-col phone'>
-              <a href="tel:08179361381">Call Now</a>
-              <a href="tel:09137819540"><b>0913 781 9540</b></a>
+              <a href="tel:07044086794">Call Now</a>
               <a href="tel:07044086794"><b>0704 408 6794</b></a>
-              <a href="tel:08179361381"><b>0817 936 1381</b></a>
+              <a href="tel:09137819540"><b>0913 781 9540</b></a>
           </div>
         </div>
 
         <div>
-          <a href="tel:08179361381"><b>Whatsapp-Only: 0817 936 1381</b></a>
+          <a href="tel:07044086794"><b>Whatsapp: 0704 408 6794</b></a>
           <div style={{color: '#4151E5', marginBottom: '10px', textAlign: 'center', marginTop: '10px', fontSize: '1.35em'}}><a href='https://www.learnnova.ng' target='blank'><b>www.learnnova.ng</b></a></div>
         </div>
         
