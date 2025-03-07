30e8b41b3b083112eec986560add3e3faf322705
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-03-07 12:55:12 +0100

    removed some things X4

--- src/layouts/MentorNigeriaSections/footerSection.js
+++ src/layouts/MentorNigeriaSections/footerSection.js
@@ -3,10 +3,10 @@ import './mentornigeria.css'
 
 const FooterSection = () => {
   return (
-    <section className='text-black px-2 sm:px-4' style={{background: '#FA8806'}}>
+    <section className='text-black px-2 sm:px-6' style={{background: '#FA8806'}}>
       <div className='flex flex-col md:flex-row  justify-between py-3'>
-        <div className='space-y-1'>
-          <h3 className='text-5xl md:text-7xl lg:text-9xl font-bold'>
+        <div>
+          <h3 className='text-xl md:text-3xl lg:text-5xl font-bold mb-2 leading-tight'>
             LearnNova Learning Support Center,
           </h3>
           <p className='text-lg md:text-xl lg:text-3xl font-semibold'>
@@ -17,8 +17,8 @@ const FooterSection = () => {
           </p>
         </div>
 
-        <div className='space-y-1'>
-          <h3 className='text-5xl md:text-7xl lg:text-9xl font-bold'>
+        <div>
+          <h3 className='text-xl md:text-3xl lg:text-5xl font-bold mb-2'>
             Email
           </h3>
           <p className='text-lg md:text-xl lg:text-3xl font-semibold'>
@@ -26,11 +26,11 @@ const FooterSection = () => {
           </p>
         </div>
 
-        <div className='space-y-1'>
-          <h3 className='text-5xl md:text-7xl lg:text-9xl font-bold'>
+        <div>
+          <h3 className='text-xl md:text-3xl lg:text-5xl font-bold mb-2'>
             <a href="tel:07044086794">Contact</a>
           </h3>
-          <ul className='list-none mx-0 px-0 font-semibold'>
+          <ul className='list-none mx-0 px-0 font-semibold text-lg md:text-xl lg:text-3xl'>
             <li><a className='hover:text-white hover:underline' href="tel:07044086794">0704 408 6794</a></li>
             <li><a className='hover:text-white hover:underline' href="tel:09137819540">0913 781 9540</a></li>
             <li><a className='hover:text-white hover:underline' href="tel:08023781682">Whatsapp: 0802 378 1682</a></li>
@@ -45,29 +45,6 @@ const FooterSection = () => {
       </div>
 
     </section>
-    // <div>
-    //   <div className='footer-section-wrapper'>
-    //     <div>
-    //       <div className='f-section-col'>
-    //           <div className='location'><b>LearnNova Learning Support Center,</b></div>
-    //           <div className='location'><b>A unit of TreNova Group.</b></div>
-    //           <div className='location'>7B, Ogunsiji Close, Off Allen Avenue, Ikeja, Lagos.</div>
-    //       </div>
-    //       <div className='f-section-col phone'>
-    //           <a href="tel:07044086794">Call Now</a>
-    //           <a href="tel:07044086794"><b>0704 408 6794</b></a>
-    //           <a href="tel:09137819540"><b>0913 781 9540</b></a>
-    //       </div>
-    //     </div>
-
-    //     <div>
-    //       <a href="tel:08023781682"><b>Whatsapp: 0802 378 1682</b></a>
-    //       <div style={{color: '#4151E5', marginBottom: '10px', textAlign: 'center', marginTop: '10px', fontSize: '1.35em'}}><a href='https://www.learnnova.ng' target='blank'><b>www.learnnova.ng</b></a></div>
-    //     </div>
-        
-    //   </div>
-        
-    // </div>
   )
 }
 
