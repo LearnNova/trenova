1488c5c0ae42a82ac3b0da758b2781632d4e76e3
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-06-17 22:19:03 +0100

    so many changes can't keep count

--- src/layouts/SchoolCareSection/SchoolCareHeroSection.jsx
+++ src/layouts/SchoolCareSection/SchoolCareHeroSection.jsx
@@ -3,8 +3,20 @@ import bannerImg from './schoolcareheroimg.webp';
 const SchoolCareHeroSection = () => {
 
   return (
-    <section className='md:mt-16'>
-        <div className="relative h-[85vh] overflow-hidden" id='hero'>
+    <section>
+        <div className='w-full flex justify-center items-center text-white'>
+            <div className='flex-1 mx-0 md:mx-10 lg:mx-15 px-2 pt-2 pb-6 text-center' style={{backgroundColor: '#1C1C1C'}}>
+                <h1 className='text-11xl md:text-17xl lg:text-21xl font-bold inline-block'>Join LearnNova Partners Network: Transform Education & Build a Profitable Future.</h1>
+                <a 
+                    className='text-lg md:text-xl lg:text-3xl font-semibold bg-orange-500 border-2  border-orange-500 rounded-md text-white hover:bg-white hover:text-orange-500 shadow-md py-2 px-3 mt-3 cursor-pointer' 
+                    style={{display: 'inline-block'}}
+                    href='#getintouch3'
+                >
+                    Enrol Now
+                </a>
+            </div>
+        </div>
+        {/* <div className="relative h-[85vh] overflow-hidden" id='hero'>
             <img 
                 src={bannerImg}
                 alt="Hero Section" 
@@ -14,9 +26,9 @@ const SchoolCareHeroSection = () => {
                 <h1 className='text-29xl lg:text-77xl font-extrabold text-orange-600 leading-tight' style={{fontFamily: 'Big Shoulders Display, sans-serif', textShadow: "2px 2px 4px rgba(0, 0, 0, 0.6)"}}>
                     Join LearnNova Partners Network: Transform Education & Build a Profitable Future.
                 </h1>
-                {/* <p className='text-9xl md:text-13xl lg:text-21xl font-medium bg-black' style={{ fontFamily: 'Satisfy, serif', textShadow: "3px 3px 6px rgba(27, 9, 9, 0.6)", color: 'rgb(251, 255, 0)', backgroundColor: "rgba(0, 0, 0, 0.25)"}}>
+                <p className='text-9xl md:text-13xl lg:text-21xl font-medium bg-black' style={{ fontFamily: 'Satisfy, serif', textShadow: "3px 3px 6px rgba(27, 9, 9, 0.6)", color: 'rgb(251, 255, 0)', backgroundColor: "rgba(0, 0, 0, 0.25)"}}>
                     Become a LearnNova School Support Partner
-                </p> */}
+                </p>
 
                 <a 
                 className='text-lg md:text-xl lg:text-3xl font-semibold bg-orange-500 rounded-md text-white hover:bg-gray-800 shadow-md py-2 px-3 mt-3 cursor-pointer' 
@@ -26,33 +38,14 @@ const SchoolCareHeroSection = () => {
                     Enrol Now
                 </a>
             </div>
-        </div>
-
-        {/* <div className='px-2 py-4'>
-            <h1 className='text-7xl md:text-9xl lg:text-13xl font-extrabold' style={{fontFamily: 'Big Shoulders Display, sans-serif'}}>
-                Elevate Your <span className='text-orange-500'>Impact</span>
-            </h1>
-            <p className='mb-4 text-lg md:text-xl lg:text-3xl'>
-                Unlock Exclusive Opportunities to Grow and Support Private Schools with the LearnNova School Success Network.
-            </p>
-
-            <a className='text-lg md:text-xl lg:text-3xl font-extrabold uppercase text-orange-500 hover:underline hover:text-white' href='#schoolbusinessopportunity' style={{fontFamily: 'Big Shoulders Display, sans-serif'}}>
-                learn more
-            </a>
         </div> */}
-
-        <div className='px-2 py-4'>
-            {/* <h1 className='text-13xl md:text-17xl lg:text-21xl font-extrabold' style={{fontFamily: 'Big Shoulders Display, sans-serif'}}>
-                Start Your <span className='text-orange-500'>Journey</span> 
-            </h1> */}
-
-            <p className='mb-4 text-3xl md:text-5xl lg:text-7xl italic'>
+        
+        <div className='w-full flex justify-center items-center text-black'>
+            <div className='flex-1 mx-0 md:mx-10 lg:mx-15 mt-4 mb-1 pt-2 pb-6 text-center'>
+                <p className='text-3xl md:text-5xl lg:text-7xl italic'>
                 Join a powerful network dedicated to enhancing education and empowering schools across Nigeria while unlocking new revenue opportunities for yourself.
             </p>
-
-            {/* <a className='text-lg md:text-xl lg:text-3xl font-extrabold uppercase text-orange-500 hover:underline hover:text-white' href='#schoolbusinessopportunity' style={{fontFamily: 'Big Shoulders Display, sans-serif'}}>
-                learn more
-            </a> */}
+            </div>
         </div>
     </section>
     
---
+++ src/layouts/SchoolCareSection/SchoolCarePackages.jsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react'
+
+const SchoolCarePackages = () => {
+    const [openPackage, setOpenPackage] = useState('');
+
+  return (
+    <ul className="list-none text-lg md:text-xl lg:text-3xl font-normal my-2 space-y-6">
+        <li className="px-2 py-3 border border-orange-600 rounded-lg shadow-sm hover:shadow-md">
+            <div className="w-full text-orange-500 font-semibold">
+                📌✅Bronze Package
+            </div>
+
+            {openPackage === 'bronze' && (
+                <div className="w-full mt-4 space-y-4">
+                    <div className="w-full">
+                        <p className='font-semibold'>✔️ Includes:</p>
+                        <p>Access to <span className="font-semibold">School Learning Support Subjects</span></p>
+                    </div>
+                    <div className="w-full">
+                        <p className="font-semibold">🎯 Partner Price:</p>
+                        <p>🔹 <span className="font-semibold">1 Subject</span> – ₦69,999 per term</p>
+                        <p>🔹 <span className="font-semibold">2 Subjects</span> – ₦99,999 per term</p>
+                    </div>
+                </div>
+            )}
+        
+            <div className="mt-2 font-medium text-slategray-100">
+
+                {openPackage === 'bronze' ? (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('')}>See Less</p>
+                ) : (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('bronze')}>See More</p>
+                )}
+        
+            </div>
+        </li>
+
+        <li className="px-2 py-3 border border-orange-600 rounded-lg shadow-sm hover:shadow-md">
+            <div className="w-full text-orange-500 font-semibold">
+                📌✅Silver Package:
+            </div>
+
+            {openPackage === 'silver' && (
+                <div className="w-full mt-4 space-y-4">
+                    <div className="w-full">
+                        <p className='font-semibold'>✔️ Includes:</p>
+                        <p>Access to <span className="font-semibold">LearnNova Home Learning App</span></p>
+                    </div>
+                    <div className="w-full">
+                        <p className="font-semibold">🎯 Per-Term Pricing:</p>
+                        <p>🔹 35 Learners – ₦59,999</p>
+                        <p>🔹 50 Learners – ₦79,999</p>
+                        <p>🔹 100 Learners – ₦149,999</p>
+                        <p>🔹 200 Learners – ₦259,999</p>
+                        <p>🔹 300 Learners – ₦359,999</p>
+                        <p>🔹 400 Learners - ₦399,999</p>
+                    </div>
+                </div>
+            )}
+        
+            <div className="mt-2 font-medium text-slategray-100">
+
+                {openPackage === 'silver' ? (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('')}>See Less</p>
+                ) : (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('silver')}>See More</p>
+                )}
+        
+            </div>
+        </li>
+
+        <li className="px-2 py-3 border border-orange-600 rounded-lg shadow-sm hover:shadow-md">
+            <div className="w-full text-orange-500 font-semibold">
+                📌✅Gold Package:
+            </div>
+
+            {openPackage === 'gold' && (
+                <div className="w-full mt-4 space-y-4">
+                    <div className="w-full">
+                        <p className='font-semibold'>✔️ Includes:</p>
+                        <p>Access to <span className="font-semibold">3 School Learning Support Studio Subjects</span></p>
+                        <p>Access for <span className="font-semibold">35</span> learners on the <span className="font-semibold">Home Learning App</span></p>
+                    </div>
+                    <div className="w-full">
+                        <p className="font-semibold">🎯 Special Partner Price:</p>
+                        <p>🔹 ₦139,999 per term</p>
+                    </div>
+                </div>
+            )}
+        
+            <div className="mt-2 font-medium text-slategray-100">
+
+                {openPackage === 'gold' ? (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('')}>See Less</p>
+                ) : (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('gold')}>See More</p>
+                )}
+        
+            </div>
+        </li>
+
+        <li className="px-2 py-3 border border-orange-600 rounded-lg shadow-sm hover:shadow-md">
+            <div className="w-full text-orange-500 font-semibold">
+                📌✅Lifetime Access Package:
+            </div>
+
+            {openPackage === 'lifetime' && (
+                <div className="w-full mt-4 space-y-4">
+                    <div className="w-full">
+                        <p className='font-semibold'>✔️ Includes:</p>
+                        <p>One-Time Payment for Lifetime Access to the School Learning Support Studio</p>
+                    </div>
+                    <div className="w-full">
+                        <p className="font-semibold">🎯 Limited Offer Price:</p>
+                        <p>🔹 1 Subject – ₦599,999</p>
+                        <p>🔹 2 Subjects – ₦999,999</p>
+                        <p>🔹 3 Subjects – ₦1,199,999</p>
+                    </div>
+                </div>
+            )}
+        
+            <div className="mt-2 font-medium text-slategray-100">
+
+                {openPackage === 'lifetime' ? (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('')}>See Less</p>
+                ) : (
+                    <p className="underline cursor-pointer" onClick={()=>setOpenPackage('lifetime')}>See More</p>
+                )}
+        
+            </div>
+        </li>              
+    </ul>
+  )
+}
+
+export default SchoolCarePackages
